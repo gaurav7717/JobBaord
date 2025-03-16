72d644efb19818511f19e611dba7bbcd38a7d5b9
gaurav7717 <gauravpradhan7hai@gmail.com>
2025-03-16 12:46:40 +0530

    front-back-link

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const [experienceInput, setExperienceInput] = useState("");
   const [availableLocations, setAvailableLocations] = useState([]);
 
-  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"; // Use env var or default
+  const API_URL = 'https://jobboard-0egu.onrender.com'; // Use env var or default
 
   const fetchJobs = useCallback(async () => {
     setLoading(true);
